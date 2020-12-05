54ec80d865e1169aaf19bc6a3a2dff56c8d4db96
Eddie Villarreal <eddie.villarreal@outlook.com>
2020-12-05 12:28:51 -0600

    tring to re delopy with new repo

---
+++ src/App.js
@@ -0,0 +1,18 @@
+  
+import React from "react";
+import Directory from "./components/directory/directory";
+import './App.css';
+
+const App = () => {
+
+  return (
+
+    <div className="App">
+      <Directory />
+    </div>
+
+  )
+
+}
+
+export default App;
